7919dcd4a553830bf981aff9d8c1a36d0d28b9ae
Kirill1111 <kirill.kozinets@mail.ru>
2018-05-19 22:02:39 +0700

    Rename program

--- Mes/Properties/AssemblyInfo.cs
+++ Mes/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("messenger")]
+[assembly: AssemblyTitle("BlockNote")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CtrlSoft")]
-[assembly: AssemblyProduct("messenger")]
+[assembly: AssemblyProduct("BlockNote")]
 [assembly: AssemblyCopyright("Copyright © CtrlSoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
