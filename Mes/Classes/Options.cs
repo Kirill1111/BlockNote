d44905a3c2a835e3a970017d22132d9561450add
Kirill1111 <kirill.kozinets@mail.ru>
2018-10-11 16:11:08 +0700

    Добавлен новый класс

---
+++ Mes/Classes/Options.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mes.Classes
+{
+    class Options
+    {
+        private static bool grammarChecker = false;
+
+        public static bool GrammarChecker {
+            get => grammarChecker;
+            set => grammarChecker = value;
+        }
+
+
+    }
+}
